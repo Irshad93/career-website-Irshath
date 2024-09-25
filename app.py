fb99b07e6dc6d62a5ff7fb109d4cb9d2c284565a
Irshad93 <mirsh939@gmail.com>
2024-09-25 23:45:01 +0530

    after added the jobs

--- app.py
+++ app.py
@@ -1,10 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id':1,
+        'title':'Data Analyst',
+        'location':'Bangaluru, India',
+        'salary': 'R.S 100,000'
+
+    },
+
+    {
+        'id':2,
+        'title':'Data Scientist',
+        'location':'Channai, India'
+
+    },
+
+    {
+        'id':3,
+        'title':'Frontend Engineer',
+        'location':'Remote',
+        'salary': 'R.S 140,000'
+
+    },
+
+    {
+        'id':4,
+        'title':'Backend Engineer',
+        'location':'St Francisco, USA',
+        'salary': '$ 120,000' 
+
+    }
+
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', 
+                            jobs=JOBS,company_name='Irshad')
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)                            
 
 if __name__== "__main__":
     app.run(host='0.0.0.0',debug=True)
